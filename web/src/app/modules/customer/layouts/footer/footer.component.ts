60102be80923f8330fa45c094af9509ef771a6df
thianchai <kanompang962@gmail.com>
2024-03-15 19:42:38 +0700

    [web]-footer components

--- web/src/app/modules/customer/layouts/footer/footer.component.ts
+++ web/src/app/modules/customer/layouts/footer/footer.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { about_shopee, app, customer_service, follow_us, logistics, payment, related_products } from 'src/assets/data/data';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+
+  related_products:any[] = [];
+  customer_service:any[] = [];
+  about_shopee:any[] = [];
+  logistics:any[] = [];
+  follow_us:any[] = [];
+  payment:any[] = [];
+  app:any[] = [];
+
+  ngOnInit(): void {
+    this.related_products = related_products;
+    this.customer_service = customer_service;
+    this.about_shopee = about_shopee;
+    this.logistics = logistics;
+    this.follow_us = follow_us;
+    this.payment = payment;
+    this.app = app;
+  }
 
 }
--- web/src/assets/data/data.ts
+++ web/src/assets/data/data.ts
@@ -33,3 +33,84 @@ export const categorys = [
     {id:5, title:"bankjak", img: "https://img.freepik.com/free-photo/regular-plain-white-tshirt-grey-background_125540-2892.jpg?t=st=1710148941~exp=1710152541~hmac=1f6e530501fc2963e21539c9bb1bd8268b1bd7321cee739822bfbb07bd3d43ad&w=2000"},
     {id:6, title:"pramata", img: "https://img.freepik.com/free-photo/regular-plain-white-tshirt-grey-background_125540-2892.jpg?t=st=1710148941~exp=1710152541~hmac=1f6e530501fc2963e21539c9bb1bd8268b1bd7321cee739822bfbb07bd3d43ad&w=2000"},
 ]
+
+export const related_products = [
+    {id:1, title:"มีไฟแอลอีดี LED บลูทูธไร้สาย ใช้ได้ทุกรุ่น TWS pro70 หูฟังบลูทูธ คุณภาพดี ใหม่ล่าสุด", },
+    {id:2, title:"JQVITEK-4-in-1-หูฟังบลูทูธ T64 TWS หูฟัง+พัดลมไฟฟ้า+พาวเวอร์แบง+ไฟฉาย มัลติฟังก์ชั่น แบตเตอร์รี่ใช้งานนาน Bluetooth 5.3", },
+    {id:3, title:"Transformers TF-T02หูฟังบลูทูธออปติมัสไพรม์หูฟังไร้สายเพลงเกมสัมผัสอัจฉริยะโหมดคู่ไมค์บลูทูธ5.3", },
+    {id:4, title:"หูฟังip foxconnแท้ไมโครโฟนในตัว เสียงที่ชัดเจนสำหรับ รุ่น i5 5s 6plus i6 6s 6splus", },
+    {id:5, title:"หูฟังมีสาย huawei เสียงดี AM115 Small Talk huawei earphone หัวเว่ย คุณภาพดี รับประกันร้าน", },
+    {id:6, title:"Moondrop CHU II หูฟังอินเอียร์ไดนามิก IEM 0.78 มม. ประสิทธิภาพสูง ถอดออกได้", },
+    {id:1, title:"หูฟัง ENYX รุ่น E1X หูฟัง Earphone 3.5mm และ Type-C เสียงดี เบสแน่น ของแท้100% มีไมโครโฟน สายสนทนา เซอร์ราวรอบทิศทาง", },
+    {id:2, title:"หูฟัง akg 3.5mm  ไมโครโฟนในตัว เล่นเกมส์ เบสหนัก สำหรับ AKG samsung s4 s5 s8 s10", },
+    {id:3, title:"KZ SK10 หูฟัง TWS 2 ไดรเวอร์ 1BA + 1DD ประกันศูนย์ไทย", },
+    {id:4, title:"Audio Technica ATH-M50XBT2 หูฟังครอบหูไร้สาย Professional Monitor Series Headphones หูฟังมอนิเตอร์ หูฟังไร้สาย", },
+    {id:5, title:"Kz AZ09 Pro หูฟังไร้สาย บลูทูธ 5.2 พร้อมเคสชาร์จ", },
+    {id:6, title:"ของแท้ TWS Pro 4 Inpods หูฟังบลูทูธไร้สาย 5.0 พร้อมไมโครโฟน ควบคุมแบบสัมผัส ลดเสียงรบกวน", },
+    {id:5, title:"TWS Wireless bluetooth 5.0 headset Earphone Earbud หูฟังบลูทูธ สเตอริโอ หูฟังเล่นเกมส์ แยกเสียงซ้ายขวา รุ่น F9-5", },
+    {id:6, title:"หูฟังไร้สาย หูฟังบลูทูธครอบหู บลูทูธ5.0 เล่นเกม/ฟังเพลง เสียง4Dลดเสียงรบกวน เสียบแมมได้ ชาร์จUSB ใช้ได้ทั้งคอมและมือถือ", },
+    {id:1, title:"หูฟัง KZ ZST X หูฟังอินเอียร์ hybrid driver 1DD+1BA kz zst x หูฟังมีไมค์ headphone สายหูฟัง หูฟังเบสหนัก earphone", },
+]
+
+export const customer_service = [
+    {id:1, title:"Help Centre", },
+    {id:2, title:"How To Buy", },
+    {id:3, title:"How To Sell", },
+    {id:4, title:"Payment Methods", },
+    {id:5, title:"Shopee Coins", },
+    {id:6, title:"Shipping & Delivery", },
+    {id:1, title:"Return & Refund", },
+    {id:2, title:"Shopee Gurantee", },
+    {id:3, title:"Contact Us", },
+]
+
+export const about_shopee = [
+    {id:1, title:"About Us", },
+    {id:2, title:"Affiliate Program", },
+    {id:3, title:"Shopee Careers", },
+    {id:4, title:"Shopee Policies", },
+    {id:5, title:"Privacy Policy", },
+    {id:6, title:"Shopee Blog", },
+    {id:1, title:"Shopee Mall", },
+    {id:2, title:"Seller Centre", },
+    {id:3, title:"Flash Deals", },
+    {id:3, title:"Media Contact", },
+]
+
+export const payment = [
+    {id:1, img:"assets/footer/shopee.png", },
+    {id:2, img:"assets/footer/scb.png", },
+    {id:3, img:"assets/footer/kbank.png", },
+    {id:1, img:"assets/footer/bay.png", },
+    {id:2, img:"assets/footer/bbl.png", },
+    {id:3, img:"assets/footer/ktb.png", },
+    {id:1, img:"assets/footer/ktc.png", },
+    {id:2, img:"assets/footer/visa.png", },
+    {id:3, img:"assets/footer/mt.png", },
+    {id:3, img:"assets/footer/jcb.png", },
+    {id:3, img:"assets/footer/up.png", },
+    {id:3, img:"assets/footer/destination.png", },
+]
+
+export const logistics = [
+    {id:1, img:"assets/footer/spx.png", },
+    {id:2, img:"assets/footer/best.png", },
+    {id:3, img:"assets/footer/j&t.png", },
+    {id:1, img:"assets/footer/dhl.png", },
+    {id:2, img:"assets/footer/kerry.png", },
+    {id:3, img:"assets/footer/post.png", },
+    {id:1, img:"assets/footer/flash.png", },
+]
+
+export const follow_us = [
+    {id:1, img:"assets/footer/facebook.png", title:"Facebook"},
+    {id:2, img:"assets/footer/ig.png", title:"Instagram"},
+    {id:3, img:"assets/footer/line.png", title:"Line"},
+    {id:4, img:"assets/footer/in.png", title:"LinkedIn"},
+]
+
+export const app = [
+    {id:1, img:"assets/footer/app_store.png", title:""},
+    {id:2, img:"assets/footer/google_play.png", title:""},  
+    {id:3, img:"assets/footer/app_gallerry.png", title:""},
+]
